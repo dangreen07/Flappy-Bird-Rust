270963f79ffc5801dc7cefc04160bab8a042075f
Daniel Green <dangreen07@outlook.com>
2025-07-03 10:38:36 +0100

    Moved Resources to a single Game Resource.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,35 @@
 use bevy::{prelude::*, sprite::Wireframe2dPlugin};
 use rand::Rng;
 
-#[derive(Resource)]
+#[derive(Default)]
 struct Player {
     entity: Option<Entity>,
     velocity: Vec3,
     acceleration: Vec3,
 }
 
-impl Default for Player {
-    fn default() -> Player {
-        Player {
-            entity: None,
-            velocity: Vec3::new(0., 0., 0.),
-            acceleration: Vec3::new(0., -20., 0.), // Gravity is enabled by default for player
-        }
-    }
-}
-
-#[derive(Default, Resource)]
+#[derive(Default)]
 struct Pipes {
     entities: Vec<Entity>,
     material: Option<MeshMaterial2d<ColorMaterial>>,
     mesh: Option<Mesh2d>,
+}
+
+#[derive(Resource)]
+struct Game {
+    player: Player,
+    pipes: Pipes,
+    floor: Option<Entity>,
+}
+
+impl Default for Game {
+    fn default() -> Game {
+        Game {
+            player: Player::default(),
+            pipes: Pipes::default(),
+            floor: None,
+        }
+    }
 }
 
 const PIPE_OFFSET: f32 = 300.;
@@ -39,9 +46,7 @@ fn main() {
 
     app.add_plugins(Wireframe2dPlugin::default());
 
-    app.init_resource::<Player>();
-
-    app.init_resource::<Pipes>();
+    app.init_resource::<Game>();
 
     app.add_systems(Startup, setup);
 
@@ -53,8 +58,7 @@ fn main() {
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut player: ResMut<Player>,
-    mut pipes: ResMut<Pipes>,
+    mut game: ResMut<Game>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     window: Query<&Window>,
@@ -69,7 +73,8 @@ fn setup(
 
     let entity = commands.spawn((sprite, transform)).id();
 
-    player.entity = Some(entity);
+    game.player.entity = Some(entity);
+    game.player.acceleration = Vec3::new(0., -20., 0.); // Set gravity acceleration here
 
     // Initialize pipes
     let Ok(window) = window.single() else {
@@ -87,8 +92,8 @@ fn setup(
     let pipe = meshes.add(Rectangle::new(PIPE_WIDTH, height));
     let mesh = Mesh2d(pipe);
 
-    pipes.material = Some(material);
-    pipes.mesh = Some(mesh);
+    game.pipes.material = Some(material);
+    game.pipes.mesh = Some(mesh);
 }
 
 fn calculate_pipe_heights(height: f32, window_height: f32) -> (f32, f32) {
@@ -102,27 +107,27 @@ fn calculate_pipe_heights(height: f32, window_height: f32) -> (f32, f32) {
 fn sprite_movement(
     time: Res<Time>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut player: ResMut<Player>,
+    mut game: ResMut<Game>,
     mut transforms: Query<&mut Transform>,
 ) {
-    let mut acceleration = player.acceleration;
+    let mut acceleration = game.player.acceleration;
     if keyboard_input.just_released(KeyCode::Space) {
         acceleration.y = JUMP_FORCE;
     }
 
-    player.velocity += acceleration * time.delta_secs();
+    game.player.velocity += acceleration * time.delta_secs();
 
     // Apply the velocity
-    let velocity = player.velocity;
-    let mut transform = transforms.get_mut(player.entity.unwrap()).unwrap();
+    let velocity = game.player.velocity;
+    let mut transform = transforms.get_mut(game.player.entity.unwrap()).unwrap();
     transform.translation += velocity * time.delta_secs();
-    *transforms.get_mut(player.entity.unwrap()).unwrap() = *transform;
+    *transforms.get_mut(game.player.entity.unwrap()).unwrap() = *transform;
 }
 
 fn pipe_update(
     time: Res<Time>,
     mut transforms: Query<&mut Transform>,
-    mut pipes: ResMut<Pipes>,
+    mut game: ResMut<Game>,
     window: Query<&Window>,
     mut commands: Commands,
 ) {
@@ -134,7 +139,7 @@ fn pipe_update(
 
     let mut max_width: f32 = 0.;
 
-    pipes.entities.retain(|pipe| {
+    game.pipes.entities.retain(|pipe| {
         let mut transform = *transforms.get_mut(*pipe).unwrap();
         let pos = transform.translation; // (width, height, z-axis) z-axis will always be zero for 2D
         if pos.x > max_width {
@@ -157,8 +162,8 @@ fn pipe_update(
         let mut rng = rand::rng();
         let random_number: f32 = rng.random_range(0.0..=1.0);
         let (top_pos, bottom_pos) = calculate_pipe_heights(random_number, size.y);
-        let mesh = pipes.mesh.clone().unwrap();
-        let material = pipes.material.clone().unwrap();
+        let mesh = game.pipes.mesh.clone().unwrap();
+        let material = game.pipes.material.clone().unwrap();
         let top_entity = commands
             .spawn((
                 mesh.clone(),
@@ -175,7 +180,7 @@ fn pipe_update(
             ))
             .id();
 
-        pipes.entities.push(top_entity);
-        pipes.entities.push(bottom_entity);
+        game.pipes.entities.push(top_entity);
+        game.pipes.entities.push(bottom_entity);
     }
 }
